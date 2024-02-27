d717d5751b113e9a6f5ec51a365ad86d0cc25366
samruddhi <samruddhisankade90251@gmail.com>
2024-02-27 06:32:54 -0800

    first commit

---
+++ demo.js
@@ -0,0 +1,13 @@
+  
+//chang value number to string
+ let a=23;
+ let b=String(a);
+ console.log(typeof(b));
+
+ //string to number
+
+ let str= "hello";
+ let num=Boolean(str)
+ 
+ console.log("datatype :"+typeof(num),str);
+ 
